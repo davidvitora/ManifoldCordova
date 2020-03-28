510850e1289668ecb5020814249ac19263fa39f5
Rob <rbalousek@hotmail.com>
2020-03-28 14:47:58 -0400

    Update updateConfigurationBeforePrepare.js

--- scripts/updateConfigurationBeforePrepare.js
+++ scripts/updateConfigurationBeforePrepare.js
@@ -112,8 +112,9 @@ function processImageList(images, baseUrl) {
 // Configure Cordova configuration parser
 function configureParser(context) {
     var cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util');
+    var ConfigParser;
     try {
-        const { ConfigParser } = require('cordova-common');
+        ConfigParser = require('cordova-common');
     } catch (err) {
         ConfigParser = context.requireCordovaModule('cordova-lib/node_modules/cordova-common').ConfigParser;
     }
